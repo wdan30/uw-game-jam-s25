51f2c977e899bdbc2b501d7acb38bafc85ccff85
wdan30 <daniwangthescientific@gmail.com>
2025-06-05 22:20:33 -0400

    added console logs

--- Assets/Scripts/MeatPlayerScript.cs
+++ Assets/Scripts/MeatPlayerScript.cs
@@ -7,7 +7,7 @@ public class MeatPlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Console.Writeline("I am the meat guy");
     }
 
     // Update is called once per frame
