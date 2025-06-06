acb6bcc10385a3261598afc336fa26baf914d1b7
wdan30 <daniwangthescientific@gmail.com>
2025-06-05 23:01:44 -0400

    Added meat boy movement

--- Assets/Scripts/MeatPlayerScript.cs
+++ Assets/Scripts/MeatPlayerScript.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MeatPlayerScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D meatBody;
+    [SerializeField] private float meatSpeed;
+    [SerializeField] private float maxSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,14 @@ public class MeatPlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        meatBody.velocity += Vector2.right * horizontalInput * meatSpeed;
+
+        if (meatBody.velocity.magnitude > maxSpeed)
+        {
+            meatBody.velocity *= maxSpeed / meatBody.velocity.magnitude;
+        }
+
     }
 }
