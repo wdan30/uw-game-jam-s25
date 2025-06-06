116f2ddfda6fc71335141999df81b298e7e48f93
Aden <adenzhu01@gmail.com>
2025-06-05 20:02:53 -0700

    Modified ice script

--- Assets/Scripts/IcePlayerScript.cs
+++ Assets/Scripts/IcePlayerScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class IcePlayerScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D iceRigidbody;
+    [SerializeField] private float iceSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    }   
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("iceHorizontal");
         
+        iceRigidbody.velocity += Vector2.right * horizontalInput * iceSpeed;
     }
 }
