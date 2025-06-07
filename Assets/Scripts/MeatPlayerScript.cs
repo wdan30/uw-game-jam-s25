d1898013b62de3d2d3f21cb194da17483b9c99f8
wdan30 <daniwangthescientific@gmail.com>
2025-06-07 00:21:13 -0400

    Adding hor movement

--- Assets/Scripts/MeatPlayerScript.cs
+++ Assets/Scripts/MeatPlayerScript.cs
@@ -9,6 +9,9 @@ public class MeatPlayerScript : MonoBehaviour
     [SerializeField] private Rigidbody2D meatBody;
     [SerializeField] private float meatSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float horizontalFriction;
+    [SerializeField] private float horizontalSpeed;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +21,36 @@ public class MeatPlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = 0;
 
-        meatBody.velocity += Vector2.right * horizontalInput * meatSpeed;
-
-        if (meatBody.velocity.magnitude > maxSpeed)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
         {
-            meatBody.velocity *= maxSpeed / meatBody.velocity.magnitude;
+            meatBody.velocity = new Vector2(0, meatBody.velocity.y);
+        } 
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            meatBody.velocity += Vector2.up * 10;
         }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInput = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInput = 1;
+        }
+
+        horizontalSpeed = meatBody.velocity.magnitude;
+
+        float currX = meatBody.velocity.x;
+        
+        meatBody.velocity += Vector2.right * horizontalInput * meatSpeed * (1 - Math.Abs(currX) / maxSpeed);
+
+        if (Math.Abs(horizontalInput) == 0)
+        {
+            float speedDamp = (Math.Abs(currX) / maxSpeed);
+            meatBody.velocity = new Vector2(currX * horizontalFriction, meatBody.velocity.y);
+        }
     }
 }
