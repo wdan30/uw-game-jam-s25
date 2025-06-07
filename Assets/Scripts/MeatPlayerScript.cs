f40487ee132b541e52843899de1d3734deac3f06
wdan30 <daniwangthescientific@gmail.com>
2025-06-07 00:56:07 -0400

    Added movement for meat

--- Assets/Scripts/MeatPlayerScript.cs
+++ Assets/Scripts/MeatPlayerScript.cs
@@ -1,16 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class MeatPlayerScript : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D meatCollider;
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Rigidbody2D meatBody;
     [SerializeField] private float meatSpeed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float horizontalFriction;
-    [SerializeField] private float horizontalSpeed;
    
     // Start is called before the first frame update
     void Start()
@@ -27,8 +29,9 @@ public class MeatPlayerScript : MonoBehaviour
         {
             meatBody.velocity = new Vector2(0, meatBody.velocity.y);
         } 
-        else if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W) && isGrounded())
         {
+            Debug.Log("Jump");
             meatBody.velocity += Vector2.up * 10;
         }
 
@@ -41,16 +44,20 @@ public class MeatPlayerScript : MonoBehaviour
             horizontalInput = 1;
         }
 
-        horizontalSpeed = meatBody.velocity.magnitude;
-
         float currX = meatBody.velocity.x;
         
         meatBody.velocity += Vector2.right * horizontalInput * meatSpeed * (1 - Math.Abs(currX) / maxSpeed);
 
         if (Math.Abs(horizontalInput) == 0)
         {
-            float speedDamp = (Math.Abs(currX) / maxSpeed);
             meatBody.velocity = new Vector2(currX * horizontalFriction, meatBody.velocity.y);
         }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(meatCollider.bounds.center, meatCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+
+        return raycastHit.collider != null;
     }
 }
